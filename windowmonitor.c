968c48abcc780ebf22098621a409fbf3e8366a32
pikkewyn <studies.tutor@gmail.com>
2016-12-14 14:09:25 +0100

    1. tested hypen 2. untested substring

--- windowmonitor.c
+++ windowmonitor.c
@@ -3,6 +3,7 @@
 //gcc gistfile1.c $(pkg-config --cflags --libs x11 xmu)
 //-L/usr/X11R6/lib -lX11
 
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -25,6 +26,71 @@
 
 Bool xerror = False;
 static bool run = true;
+
+char* longest_common_string( char* string1, char* string2 ) 
+{
+	int strlen1 = strlen( string1 );
+	int strlen2 = strlen( string2 );
+
+	int len = strlen1 < strlen2 ? strlen1 : strlen2;
+	int longest = 0;
+	
+  int **ptr = malloc( 2 * sizeof( int* ) );
+	
+  int* ret = calloc( strlen1 + 1, sizeof( int ) );
+
+  for( int i = 0; i < 2; i++ )
+		ptr[ i ] = calloc( strlen2, sizeof( int ) );
+
+	int k = 0;
+	
+  for( int i = 0; i < strlen1; i++ ) 
+  {
+		memcpy( ptr[0], ptr[1], strlen2 * sizeof( int ) );
+		for (int j = 0; j < strlen2; j++) 
+    {
+			if (string1[i] == string2[j]) 
+      {
+				if (i == 0 || j == 0) 
+        {
+					ptr[1][j] = 1;
+				} else 
+        {
+					ptr[1][j] = ptr[0][j-1] + 1;
+				}
+				if (ptr[1][j] > longest) 
+        {
+					longest = ptr[1][j];
+					k = 0;
+					ret[k++] = longest;
+				}
+				if (ptr[1][j] == longest) 
+        {
+					ret[k++] = i;
+					ret[k] = -1;
+				}
+			} else 
+      {
+				ptr[1][j] = 0;
+			}
+		}
+	}
+
+	for (int i = 0; i < 2; i++)
+		free(ptr[i]);
+	free(ptr);
+
+	ret[0] = longest;
+	
+  char* result = calloc( strlen1, 1 ); 
+  
+	for( int i = 0; ret[++i] != -1; ) 
+  {
+		sprintf( result, "%.*s", longest, &string1[ret[i] - longest+1] );
+	}
+
+  return result;
+}
 
 Display* open_display()
 {
@@ -248,9 +314,39 @@ int LongestCommonSubstring( char const* str1, char const* str2 )
      return maxSubstr;
 }
 
+bool hyphen_split( char const* input, char* before, char* after )
+{
+  char* hyphen = strrchr( input, '-' );
+
+  if( hyphen == NULL || *( hyphen + 1 ) == '\0' )
+  {
+    return false;
+  }
+
+  return true;
+}
+
+void test_hypen_split()
+{
+  {
+    char const* text = "- ";
+    char* description = NULL;
+    char* name = NULL;
+    assert( hyphen_split( text, description, name ) == true );
+  }
+  {
+    char const* text = "-";
+    char* description = NULL;
+    char* name = NULL;
+    assert( hyphen_split( text, description, name ) == false );
+  }
+}
+
 int main( void )
 {
-	if( set_sigint_action() == false )
+  test_hypen_split();
+	
+  if( set_sigint_action() == false )
 			return EXIT_FAILURE;
 
 	Display* d;
